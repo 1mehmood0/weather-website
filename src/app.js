8e6f474962829480bf869b2229bbceba231bf166
1pervysage0 <msyed7008@gmail.com>
2022-02-09 02:57:37 +0530

    Partials removed

--- src/app.js
+++ src/app.js
@@ -8,15 +8,20 @@ const port=process.env.PORT||8000;
 const dirPath=path.join(__dirname,"../public");
 
 //views folder path
-const hbsPath=path.join(__dirname,"../tempelates/views");
+const hbsPath=path.join(__dirname,"../views");
+const partPath=path.join(__dirname,"../views/partials");
 app.use(express.static(dirPath));
 
 //setting tempelate engine
 app.set('view engine','hbs');
-app.set('views',hbsPath);
-hbs.registerPartials("../tempelates/partials");
+
+//app.set('views',hbsPath);
+
 //to call css static page from public
 app.use(express.static(dirPath));
+hbs.registerPartials("../tempelates/views/partials",()=>{
+    console.log("partials regisetred at ",partPath);
+});
 
 //routing
 app.get("/",(req,res)=>{
